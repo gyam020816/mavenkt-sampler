69d6fd88143fa2568693cd7e1ce495804fc54a93
Georges Yam <georges.yam@vidal.fr>
2019-03-13 12:20:59 +0100

    Start

---
+++ core/src/test/kotlin/gr/GildedRoseTest.kt
@@ -0,0 +1,12 @@
+package gr
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+internal class GildedRoseTest {
+
+    @Test
+    internal fun `it should`() {
+        Assertions.assertThat(true).isFalse()
+    }
+}
